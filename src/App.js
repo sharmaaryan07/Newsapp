f9e06c46767d3698fa6035c4c62a576b83bbf054
Aryan Sharma <sharmaaryan0828@gmail.com>
2022-05-18 11:50:33 +0530

    Added few components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from "./component/Navbar";
+// import About from "./component/About";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import About from './component/About';
+import Newsitem from './component/Newsitem';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Navbar title="NewsApp" />
+        <Routes>
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </Router>
+      <Newsitem />
     </div>
   );
 }
---
+++ src/component/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function navbar(props) {
+    return (
+        <div>
+            <header class="text-white  bg-gray-900 body-font">
+                <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+                    <a class="flex title-font font-medium items-center text-white mb-4 md:mb-0" href="/">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
+                            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+                        </svg>
+                        <span class="ml-3 text-xl">{props.title}</span>
+                    </a>
+                    <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+                        <a class="mr-5 hover:text-white" href="/">First Link</a>
+                        <a class="mr-5 hover:text-white" href="/">Second Link</a>
+                        <a class="mr-5 hover:text-white" href="/">Third Link</a>
+                        <Link class="mr-5 hover:text-white" to="/about">About Us</Link>
+                    </nav>
+                    <button class="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">Button
+                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
+                            <path d="M5 12h14M12 5l7 7-7 7"></path>
+                        </svg>
+                    </button>
+                </div>
+            </header>
+        </div>
+    )
+}
