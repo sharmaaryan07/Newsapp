d2531b07dea84272468ee8ce775e49cef9b62f8f
Aryan Sharma <sharmaaryan0828@gmail.com>
2022-05-18 12:48:38 +0530

    Added Dark Mode

--- src/App.js
+++ src/App.js
@@ -8,17 +8,35 @@ import {
 } from "react-router-dom";
 import About from './component/About';
 import Newsitem from './component/Newsitem';
+import { useState } from 'react';
 
 function App() {
+
+  const [mode,setMode]= useState('white')
+  let toggleDarkMode=()=>{
+    if(mode === 'white'){
+      setMode('gray-900');
+      document.body.style='background: #111827; color: white ';
+    }
+    else{
+      setMode('white')
+      document.body.style="backgorund: white; color: black";
+    }
+  }
+
+
   return (
     <div>
       <Router>
-        <Navbar title="NewsApp" />
+        <Navbar title="NewsApp"  toggleDarkMode={toggleDarkMode} mode={mode}/>
         <Routes>
+          <Route path='/' element={<Newsitem />} />
           <Route path='/about' element={<About />} />
+
         </Routes>
       </Router>
-      <Newsitem />
+      
+      
     </div>
   );
 }
