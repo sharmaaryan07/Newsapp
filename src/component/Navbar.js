d56552fbf5f4985b4c3740ecfaf9f0a08ab7adb8
Aryan Sharma <sharmaaryan0828@gmail.com>
2022-05-21 10:51:15 +0530

    Changing my code to rcc

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,29 +1,31 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { Component } from 'react'
 
-export default function navbar(props) {
-    return (
-        <div>
-            <header className={`text-${props.mode==='white'?'gray-600':'white'} bg-${props.mode}  body-font`}>
-                <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-                    <a className="flex title-font font-medium items-center ext-gray-900 mb-4 md:mb-0" href="/">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-                            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-                        </svg>
-                        <span className="ml-3 text-xl">{props.title}</span>
-                    </a>
-                    <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-                        <Link className={`mr-5 hover:${props.mode==='white'?'text-blue-500':'text-blue-500'}`} to="/">Home</Link>
-                        <Link className="mr-5" to="/">Second Link</Link>
-                        <Link className="mr-5" to="/">Third Link</Link>
-                        <Link className="mr-5" to="/about">About Us</Link>
-                    </nav>
-                    <button className={`inline-flex items-center ${props.mode==='white'?'bg-gray-200':'bg-[#6366f1]'} border-0 py-1 px-3 focus:outline-none hover:${props.mode==='white'?'bg-gray-300':'bg-blue-500'} rounded 
-                    text-base mt-4 md:mt-0`} onClick={props.toggleDarkMode}> {props.mode==='white'?'Dark':'Light'} Mode
-                    </button>
-                </div>
-            </header>
-            <hr className='bg-indigo-500'/>
-        </div>
-    )
+export default class navbar extends Component {
+    render() {
+        return (
+            <div>
+                <header className="text-gray-400 bg-gray-900 body-font">
+                    <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+                        <a className="flex title-font font-medium items-center text-white mb-4 md:mb-0" href="/">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
+                                <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+                            </svg>
+                            <span className="ml-3 text-xl">Tailblocks</span>
+                        </a>
+                        <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+                            <a className="mr-5 hover:text-white" href="/">First Link</a>
+                            <a className="mr-5 hover:text-white" href="/">Second Link</a>
+                            <a className="mr-5 hover:text-white" href="/">Third Link</a>
+                            <a className="mr-5 hover:text-white" href="/">Fourth Link</a>
+                        </nav>
+                        <button className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">Button
+                            <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
+                                <path d="M5 12h14M12 5l7 7-7 7"></path>
+                            </svg>
+                        </button>
+                    </div>
+                </header>
+            </div>
+        )
+    }
 }
--- src/component/Newsitem.js
+++ src/component/Newsitem.js
@@ -1,46 +1,30 @@
 import React from 'react'
 
 export default function Newsitem(props) {
+
+    
+      
     return (
         <div>
             <section className={`text-gray-400 body-font ${props.mode === 'white' ? 'white' : 'bg-gray-900'} mt-9 mx-2`}>
                 <div className="flex flex-wrap">
                     <div className="xl:w-1/4 md:w-1/2 p-4" >
-                        <div className={`${props.mode === 'white' ? 'bg-gray-100' : 'bg-gray-800'} hover:shadow-lg  hover:${props.mode === 'white' ? 'shadow-gray-700' : 'shadow-indigo-500'} bg-opacity-40 p-6 rounded-lg`}>
+                        <div className={`${props.mode === 'white' ? 'bg-gray-100' : 'bg-gray-800'} hover:shadow-lg ${props.mode === 'white' ? ' hover:shadow-gray-700' : ' hover:shadow-indigo-500'} bg-opacity-40 p-6 rounded-lg`}>
                             <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/720x400" alt="content" />
-                            <h3 className="tracking-widest text-indigo-400 text-xs font-medium title-font">SUBTITLE</h3>
-                            <h2 className={`text-lg ${props.mdoe === 'white' ? 'text-gray-900' : 'white'} font-medium title-font mb-4`}>Chichen Itza</h2>
-                            <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
-                        </div>
-                    </div>
 
-                    <div className="xl:w-1/4 md:w-1/2 p-4" >
-                        <div className={`${props.mode === 'white' ? 'bg-gray-100' : 'bg-gray-800'} hover:shadow-lg  hover:${props.mode === 'white' ? 'shadow-gray-700' : 'shadow-indigo-500'} bg-opacity-40 p-6 rounded-lg`}>
-                            <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/721x401" alt="content" />
-                            <h3 className="tracking-widest text-indigo-400 text-xs font-medium title-font">SUBTITLE</h3>
-                            <h2 className={`text-lg ${props.mdoe === 'white' ? 'text-gray-900' : 'white'} font-medium title-font mb-4`}>Colosseum Roma</h2>
-                            <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
-                        </div>
-                    </div>
-
-                    <div className="xl:w-1/4 md:w-1/2 p-4" >
-                        <div className={`${props.mode === 'white' ? 'bg-gray-100' : 'bg-gray-800'} hover:shadow-lg  hover:${props.mode === 'white' ? 'shadow-gray-700' : 'shadow-indigo-500'} bg-opacity-40 p-6 rounded-lg`}>
-                            <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/722x402" alt="content" />
-                            <h3 className="tracking-widest text-indigo-400 text-xs font-medium title-font">SUBTITLE</h3>
-                            <h2 className={`text-lg ${props.mdoe === 'white' ? 'text-gray-900' : 'white'} font-medium title-font mb-4`}>Great Pyramid of Giza</h2>
-                            <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
-                        </div>
-                    </div>
-
-                    <div className="xl:w-1/4 md:w-1/2 p-4" >
-                        <div className={`${props.mode === 'white' ? 'bg-gray-100' : 'bg-gray-800'} hover:shadow-lg  hover:${props.mode === 'white' ? 'shadow-gray-700' : 'shadow-indigo-500'} bg-opacity-40 p-6 rounded-lg`}>
-                            <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/723x403" alt="content" />
-                            <h3 className="tracking-widest text-indigo-400 text-xs font-medium title-font">SUBTITLE</h3>
-                            <h2 className={`text-lg ${props.mdoe === 'white' ? 'text-gray-900' : 'white'} font-medium title-font mb-4`}>San Francisco</h2>
+                            <h3 className="tracking-widest text-indigo-400 text-xs font-medium title-font">
+                                SUBTITLE
+                            </h3>
+                            
+                            <h2 className={`text-lg ${props.mdoe === 'white' ? 'text-gray-900' : 'white'} font-medium title-font mb-4`}>
+                                Chichen Itza
+                            </h2>
+                            
                             <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
                         </div>
                     </div>
                 </div>
+                    
             </section>
         </div>
     )
